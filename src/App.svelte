b3cd504489230ac3943c713cdf5aa86ad5b9f8d5
Dominik Pilipczuk <63516830+hrbio@users.noreply.github.com>
2023-03-26 01:50:55 +0100

    Added random teams

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-	import "./styles/global.css";
+	import './styles/global.css';
+	import {
+		availableChampions,
+		fetchData,
+		type championType,
+	} from './utils/fetchData';
+	import { randomTeam } from './utils/randomTeam';
+
+	fetchData();
+
+	let teamOne: championType[];
+	let teamTwo: championType[];
+
+	$: $availableChampions, (() => {
+		teamOne = randomTeam($availableChampions)
+		teamTwo = randomTeam($availableChampions)
+	})();
 </script>
 
-<h1>Hello, world!</h1>
+<main class="flex flex-col items-center justify-center">
+	<h1>Random League of Legends draft</h1>
+	{#each teamOne as champ}
+		<p>{champ.name}</p>
+	{/each}
+	<p>vs</p>
+	{#each teamTwo as champ}
+		<p>{champ.name}</p>
+	{/each}
+</main>
